437f2b63f3bd5a93210e0067e10681d4c3daf9d3
rajszym <rajszym@o2.pl>
2021-03-23 21:44:04 +0100

    updated hal drivers

---
+++ intros/cmake/config-stm32l4discovery-gcc.cmake
@@ -0,0 +1,5 @@
+include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/config-stm32l4discovery-gcc.cmake")
+
+set(INTROS_ARCH     "cortexm")
+set(INTROS_DEVICE   "stm32l4")
+set(INTROS_COMPILER "gnucc")
---
+++ stateos/cmake/config-stm32l4discovery-gcc.cmake
@@ -0,0 +1,5 @@
+include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/config-stm32l4discovery-gcc.cmake")
+
+set(STATEOS_ARCH     "cortexm")
+set(STATEOS_DEVICE   "stm32l4")
+set(STATEOS_COMPILER "gnucc")
