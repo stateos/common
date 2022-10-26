2c16ab3b3c620b9df476b56bd4adb3abdbbc0325
rajszym <rajszym@o2.pl>
2022-10-26 17:58:23 +0200

    intros: updated kernel

--- intros/kernel/osbase.h
+++ intros/kernel/osbase.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: osbase.h
     @author  Rajmund Szymanski
-    @date    26.07.2022
+    @date    26.10.2022
     @brief   This file contains basic definitions for IntrOS.
 
  ******************************************************************************
@@ -92,6 +92,19 @@ extern "C" {
 #endif
 
 /* -------------------------------------------------------------------------- */
+
+#if     HW_TIMER_SIZE == 16
+typedef uint16_t     hwt_t;
+#define HWT_MAX           0xFFFFU
+#elif   HW_TIMER_SIZE == 32
+typedef uint32_t     hwt_t;
+#define HWT_MAX           0xFFFFFFFFUL
+#elif   HW_TIMER_SIZE == 64
+typedef uint64_t     hwt_t;
+#define HWT_MAX           0xFFFFFFFFFFFFFFFFULL
+#elif   HW_TIMER_SIZE
+#error  Invalid HW_TIMER_SIZE value!
+#endif
 
 #if     OS_TIMER_SIZE == 16
 typedef uint16_t     cnt_t;
